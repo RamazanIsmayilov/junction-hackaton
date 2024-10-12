0cd6fc4683ace4da00757ffc36d383c59e702b83
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2024-10-13 00:03:39 +0400

    adding router

---
+++ src/Page/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='mx-auto container '>
+      <section>
+        <h1></h1>
+        <div className='h-full border border-red-500'></div>
+      </section>
+      <section></section>
+    </div>
+  )
+}
+
+export default Search
